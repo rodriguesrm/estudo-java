985dea5b7ec0749639d7225a052d75e9a336b2b6
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2022-10-14 10:20:26 -0300

    Entrada de dados

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        exercicioPrintfFixaxao();
+        entradaDeDados();
+//        exercicioConcatenacao();
+
     }
 
-    private static void exercicioPrintfFixaxao(){
+    /**
+     * Exercício de Concatenação
+     */
+    private static void exercicioConcatenacao(){
 
         String product1 = "Computer";
         String product2 = "Office Desk";
@@ -35,6 +41,50 @@ public class Main {
 
         Locale.setDefault(Locale.US);
         System.out.printf("US decimal point %.3f%n", measure);
+
+    }
+
+
+    /**
+     * Atividade de entrada de dados
+     */
+    private static void entradaDeDados() {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        System.out.print("Informe o seu nome Completo: ");
+        nome = sc.nextLine();
+
+        String pronome;
+        System.out.print("Como você gostaria de ser tratado (pronome): ");
+        pronome = sc.next();
+
+        char genero;
+        System.out.print("Informe seu gênero (M)asculino/(F)eminino: ");
+        genero = sc.next().charAt(0);
+
+        int idade;
+        System.out.print("Informe sua idade? ");
+        idade = sc.nextInt();
+
+        double salario;
+        System.out.print("Informe seu salario? ");
+        salario = sc.nextDouble();
+
+        String cidade;
+        System.out.print("Informe a cidade que você mora: ");
+        if (sc.hasNextLine())
+            sc.nextLine();
+        cidade = sc.nextLine();
+
+        Locale currentLocate = Locale.getDefault();
+        Locale.setDefault(new Locale("pt", "BR"));
+        System.out.printf("Olá %s %s%nVocê é do sexo %s, tem %d anos e seu salário é R$ %.2f.%nVocê mora em %s", pronome, nome, genero, idade, salario, cidade);
+        Locale.setDefault(currentLocate);
+
+        sc.close();
 
     }
 
