1bf6ef069bf32dca0d7fd671677c13d4e973f4ce
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2022-11-11 08:06:37 -0300

    Iniciado exercicio proposto aula 99

--- 02-poo/src/exercicios/listas/EmpStore.java
+++ 02-poo/src/exercicios/listas/EmpStore.java
@@ -3,34 +3,92 @@ package exercicios.listas;
 import entities.Emplyoee;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class EmpStore {
 
     // region Local methods
 
-    private static void include(List<Emplyoee> emplyoeeList) {
+    private static void waitToEnter(Scanner sc) {
+        System.out.print("Pressione <ENTER> tecla para continuar...");
+        clearBuffer(sc);
+        sc.nextLine();
+    }
 
-        try (Scanner sc = new Scanner(System.in)) {
+    private static void seedData(List<Emplyoee> emplyoeeList) {
+        emplyoeeList.add(new Emplyoee(1, "RODRIGO", 16778.67));
+        emplyoeeList.add(new Emplyoee(2, "LUCIENE", 1300.00));
+        emplyoeeList.add(new Emplyoee(3, "AMANDA", 1800.00));
+        emplyoeeList.add(new Emplyoee(4, "TIAGO", 1800.00));
+    }
 
-            System.out.print("+----------------------------------------------------------------------------+");
+    private static void clearBuffer(Scanner sc) {
+        if (sc.hasNextLine())
+            sc.nextLine();
+    }
 
-            System.out.printf("%nId.......: ");
-            int id = sc.nextInt();
+    private static void printEmploye(Emplyoee emplyoee) {
+        System.out.print("Id.......: ");
+        System.out.println(emplyoee.getId());
 
-            System.out.printf("%nNome.....: ");
-            String name = sc.nextLine();
+        System.out.print("Nome.....: ");
+        System.out.println(emplyoee.getName());
 
-            System.out.printf("%nSalário..: ");
-            if (sc.hasNextLine())
-                sc.nextLine();
-            double salary = sc.nextDouble();
+        System.out.print("Salário..: ");
+        System.out.printf("%n%.2f%n%n", emplyoee.getSalary());
+    }
 
-            if (emplyoeeList.stream().filter(x -> x.getId() == id).findAny().isEmpty())
-                emplyoeeList.add(new Emplyoee(id, name, salary));
+    private static void listaAll(Scanner sc, List<Emplyoee> emplyoeeList) {
+        for (Emplyoee emplyoee : emplyoeeList) {
+            System.out.println("------------------------------------------------------------------------------");
+            printEmploye(emplyoee);
+        }
+        waitToEnter(sc);
+    }
+
+    private static void include(Scanner sc, List<Emplyoee> emplyoeeList) {
+
+        System.out.print("+----------------------------------------------------------------------------+");
+
+        System.out.printf("%nId.......: ");
+        int id = sc.nextInt();
+
+        System.out.printf("Nome.....: ");
+        clearBuffer(sc);
+        String name = sc.nextLine();
+
+        System.out.printf("Salário..: ");
+        double salary = sc.nextDouble();
+
+        if (emplyoeeList.stream().filter(x -> x.getId() == id).findAny().isEmpty())
+            emplyoeeList.add(new Emplyoee(id, name, salary));
+        else
             System.out.println("Id já cadastrado");
 
+    }
 
+    private static void locate(Scanner sc, List<Emplyoee> emplyoeeList) {
+
+        System.out.print("+----------------------------------------------------------------------------+");
+
+        System.out.printf("%nInforme um Id para buscar: ");
+        int id = sc.nextInt();
+
+        Emplyoee emplyoee = emplyoeeList
+                .stream()
+                .filter(x -> x.getId() == id)
+                .findAny()
+                .orElse(null);
+
+        if (emplyoee == null) {
+            System.out.println("Registro não encontrado");
+        } else {
+
+            printEmploye(emplyoee);
+            waitToEnter(sc);
+
+            System.out.printf("%n%n");
         }
 
     }
@@ -42,36 +100,47 @@ public class EmpStore {
     /** Método principal para chamada */
     public static void open() {
 
+        Locale.setDefault(Locale.US);
         boolean doContinue;
         List<Emplyoee> emplyoees = new ArrayList<Emplyoee>();
 
-        do {
+        seedData(emplyoees);
 
-            doContinue = true;
+        try (Scanner sc = new Scanner(System.in)) {
+            do {
 
-            System.out.println("+----------------------------------------------------------------------------+");
-            System.out.println("| ESCOLHA UMA OPCAO                                                          |");
-            System.out.println("+----------------------------------------------------------------------------+");
-            System.out.println("| 1 - Cadastrar                                                              |");
-            System.out.println("| 2 - Consultar                                                              |");
-            System.out.println("| 3 - Relatorio                                                              |");
-            System.out.println("| 0 - SAIR                                                                   |");
-            System.out.println("+----------------------------------------------------------------------------+");
+                doContinue = true;
 
-            int menuChoice;
-            try (Scanner sc = new Scanner(System.in)) {
+                System.out.println("+----------------------------------------------------------------------------+");
+                System.out.println("| ESCOLHA UMA OPCAO                                                          |");
+                System.out.println("+----------------------------------------------------------------------------+");
+                System.out.println("| 1 - Cadastrar                                                              |");
+                System.out.println("| 2 - Consultar                                                              |");
+                System.out.println("| 3 - Relatorio                                                              |");
+                System.out.println("| 0 - SAIR                                                                   |");
+                System.out.println("+----------------------------------------------------------------------------+");
+
+                int menuChoice;
+
                 menuChoice = sc.nextInt();
-            }
 
-            switch (menuChoice) {
-                case 1:
-                    include(emplyoees);
-                    break;
-                default:
-                    doContinue = false;
-            }
+                switch (menuChoice) {
+                    case 1:
+                        include(sc, emplyoees);
+                        break;
+                    case 2:
+                        locate(sc, emplyoees);
+                        break;
+                    case 3:
+                        listaAll(sc, emplyoees);
+                        break;
+                    default:
+                        doContinue = false;
+                }
 
-        } while (doContinue);
+            } while (doContinue);
+
+        }
 
         System.out.println("FIM DO PROGRAMA");
 
