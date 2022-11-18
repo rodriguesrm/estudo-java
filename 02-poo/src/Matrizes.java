57518422d19284814ec3ccdd89f61b93a4d42aae
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2022-11-18 08:25:42 -0300

    Iniciado aula de matrizes

---
+++ 02-poo/src/Matrizes.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Matrizes {
+
+    public static void exercicioResolvido() {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            int n = sc.nextInt();
+
+            int[][] mat = new int[n][n];
+
+        }
+
+    }
+
+}
